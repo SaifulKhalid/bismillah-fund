686994e620f0ee10619922bc8c5999b5c1ff17d6
Khalid Saifullah <79466923+SaifulKhalid@users.noreply.github.com>
2025-07-18 02:24:06 +0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCrmiJJ3cAsXI7XhO4oLdKi3JfsFJKVRHs",
+  authDomain: "bf-lddb.firebaseapp.com",
+  projectId: "bf-lddb",
+  storageBucket: "bf-lddb.firebasestorage.app",
+  messagingSenderId: "454725529057",
+  appId: "1:454725529057:web:1e5240badc04232a8636a0",
+  measurementId: "G-0MNDGM5V4M"
+};
